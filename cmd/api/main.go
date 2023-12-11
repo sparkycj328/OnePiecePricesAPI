628ee5e3bb8ea1c5bd095ab98b4f4fe788896103
Carlos <sparkycj328@gmail.com>
2023-12-11 01:47:50 +0100

    Add initial structure including healthcheck endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// currently manually set, but will be generated automatically when we build
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+// For now, the only configuration settings will be the network port that we want the
+// server to listen on, and the name of the current operating environment for the
+// application (development, staging, production, etc.). We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware. At the moment this only contains a copy of the config struct and a
+// logger, but it will grow to include a lot more as our build progresses.
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	//declare an instance of the config struct
+	var (
+		cfg      config
+		portAddr = ":%d"
+	)
+
+	// Read the passed terminal flags and parse them
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "environment", "dev", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize the structured logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// initialize the application struct with our config and logger
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
+	// to the healthcheckHandler method (which we will create in a moment).
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthCheckHandler)
+
+	// Declare a HTTP server which listens on the port provided in the config struct,
+	// uses the servemux we created above as the handler, has some sensible timeout
+	// settings and writes any log messages to the structured logger at Error level.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(portAddr, cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// start the http server
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/sparkycj328/PokedexAPI
 
-go 1.21.4
+go 1.21.5
